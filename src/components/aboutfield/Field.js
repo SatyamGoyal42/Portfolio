9368a6709d1f9873d7ab94dfd25f959b40f1e75c
Satyam goyal <satyam1goyal1@gmail.com>
2024-08-18 20:37:26 +0530

    final site

--- src/components/aboutfield/Field.js
+++ src/components/aboutfield/Field.js
@@ -7,8 +7,8 @@ const Field = (props) => {
         <div className='field'>
         <div className='title'> Education</div>
       <Indf insti = "Delhi Technological University" date = "2021-2025" desc = "B.tech - Computer Engineering - CGPA 7.09"/>
-      <Indf insti = "Jayshree Periwal High School" date = "2021" desc = "Class 12 - CBSE Boards - Percentage – 95.6%"/>
-      <Indf insti = "St.Anselm’s North City Schoo" date = "2019" desc = "Class 10 - CBSE Boards - Percentage – 95.4%"/>
+      <Indf insti = "Jayshree Periwal High School" date = "2021" desc = "Class 12 - CBSE Boards - Percentage - 95.6%"/>
+      <Indf insti = "St.Anselm’s North City School" date = "2019" desc = "Class 10 - CBSE Boards - Percentage - 95.4%"/>
 
     </div>
     
