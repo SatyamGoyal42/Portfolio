dd6443d0e5a375fdf3fc7830b40bf5643f2ddd1a
Satyam goyal <satyam1goyal1@gmail.com>
2024-08-18 20:55:47 +0530

    apt changes for vercel

--- src/components/aboutfield/Field.js
+++ src/components/aboutfield/Field.js
@@ -4,7 +4,7 @@ import Indf from './Indf'
 const Field = (props) => {
   return (
     <div className='super'>
-        <div className='field'>
+        <div className='field1'>
         <div className='title'> Education</div>
       <Indf insti = "Delhi Technological University" date = "2021-2025" desc = "B.tech - Computer Engineering - CGPA 7.09"/>
       <Indf insti = "Jayshree Periwal High School" date = "2021" desc = "Class 12 - CBSE Boards - Percentage - 95.6%"/>
@@ -12,7 +12,7 @@ const Field = (props) => {
 
     </div>
     
-    <div className='field'>
+    <div className='field2'>
         <div className='title'> Experience</div>
         <Indf insti = "Web Developer Intern | JDB Infotech." date = "July 2024 - Present" desc = ""/>
         <Indf insti = "SDE Intern | EXL Services" date = "Dec 2023 - Mar 2024" desc = ""/>
